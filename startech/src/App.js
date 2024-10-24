2717f86e92e0832371da8542b39b18ccc7fdfb09
Ellen172 <ellencas172@gmail.com>
2024-10-24 14:51:54 -0300

    iniciando projeto; criando tela inicial;

---
+++ startech/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import fundo from './img/fundo.jpg';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="fundo-principal">
+        <div className='titulo-principal'>
+          <h1>StarTech</h1>
+          <h2>Inovação que transforma o futuro</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
